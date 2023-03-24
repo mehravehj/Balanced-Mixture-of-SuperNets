9efe6bbcc81b934039a1bd70a09b37d213141cd1
mehravehj <54187772+mehravehj@users.noreply.github.com>
2023-03-24 03:07:35 -0400

    Update split_dataset.py

--- venv/respo/Food101/split_dataset.py
+++ venv/respo/Food101/split_dataset.py
@@ -37,28 +37,3 @@ if __name__ == '__main__':
         if not os.path.isdir(image_tar_dir_train):
            os.makedirs(image_tar_dir_train)
         copyFile(image_dir, image_tar_dir_test, image_tar_dir_train)
-
-
-
-######creat train.txt and test.txt to split_datasets
-    # path = './VOC2012/Annotations'
-    # img_list = os.listdir(path)
-    # txt_path = './VOC2012/ImageSets/Main'
-    # if not os.path.exists(txt_path):
-    #    os.makedirs(txt_path)
-    #
-    # lengths_lists = len(img_list)
-    # test_names = random.sample(img_list, int(lengths_lists * TEST_PERCENTAGE / 100))
-    # # filename = './test'
-    # with open(txt_path + '/trainval.txt', 'w') as f:
-    #     for image_id in img_list:
-    #         if image_id in test_names:
-    #             continue
-    #         else:
-    #             image_id = image_id.split('.')[0]
-    #             f.write('%s\n' % image_id)
-    # with open(txt_path + '/test.txt', 'w') as f:
-    #     for image_id in test_names:
-    #         image_id = image_id.split('.')[0]
-    #         f.write('%s\n' % image_id)
-
