eb209ffc81877b86be7ce2544e8fc74e0382a2a0
mehravehj <54187772+mehravehj@users.noreply.github.com>
2023-03-23 23:51:03 -0400

    Delete MultimodelNAS/respo directory

--- MultimodelNAS/respo/cifar/resnet50/utils/utility_functions.py
+++
@@ -1,8 +0,0 @@
-def string_to_list(x, leng):
-    if ',' in x:
-        x = x.split(',')
-        res = [int(i) for i in x]
-    else:
-        res = [int(x) for i in range(leng-1)]
-
-    return res
