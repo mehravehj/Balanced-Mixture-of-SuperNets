2782946b4130bfa7034c9d21b017d728961c74a3
mehravehj <54187772+mehravehj@users.noreply.github.com>
2023-03-25 02:25:15 -0400

    Update NAS_trainer.py

--- venv/respo/cifar/resnet50/utils/NAS_trainer.py
+++ venv/respo/cifar/resnet50/utils/NAS_trainer.py
@@ -16,7 +16,6 @@ def create_models(num_models):
     create n identical models
     """
     models = [ResNet(Bottleneck, [3, 4, 6, 3]) for _ in range(num_models)]  # create multiple models
-    # model2 = copy.deepcopy(model1) # do I make them same initialization?
     return models
 
 
@@ -50,32 +49,21 @@ def train_valid(models, train_queue, optimizers, sample_weights, paths, weight_m
                 decay, counter_matrix, threshold, scaler, fp32,
                 criterion=nn.CrossEntropyLoss()):
     p_m = []
-    # print(weight_mat.size())
     prob = torch.nn.functional.softmax(weight_mat * temperature, dim=1)  # conditional prob p(model|arch)
     marginal = torch.sum(prob, 0).view(1, -1) / prob.size(0)  # sum / 36 size is (1,2)
-    # print(marginal.size())
     num_models = len(models)
     # define uniform probability
     marginal_uniform = torch.nn.functional.softmax(torch.ones(1, num_models), dim=1)
-    # print(marginal_uniform.size())
-    # calculate KL divergence
-    # print(marginal)
-    # print(marginal_uniform)
     kl_dis = abs((marginal * (marginal / marginal_uniform).log()).sum())
     print(kl_dis)
     while kl_dis > threshold:
-        # print('calculated marginal', marginal)
         p_m.append(marginal)
         weighted_p = prob / marginal  # Calculate
-        # print(weighted_p)
         # normalize conditional probs over models
         prob = weighted_p / torch.sum(weighted_p, 1).view(weighted_p.size(0), 1)
-        # print(prob.size())
         marginal = torch.sum(prob, 0).view(1, -1) / prob.size(0)  # marginal
         # update KL distance
         kl_dis = abs((marginal * (marginal / marginal_uniform).log()).sum())
-        # print(kl_dis)
-        # print(marginal)
 
     # initializing average per epoch metrics
     train_loss = 0  # average train loss
@@ -99,19 +87,13 @@ def train_valid(models, train_queue, optimizers, sample_weights, paths, weight_m
         model.module.set_path(pool)  # setting path
         optimizer = optimizers[model_index]
         model.train()
-        # print('path:', path_index, pool)
-        # print(model)
-        # train_inputs, train_targets = train_inputs.cuda(), train_targets.cuda()
         optimizer.zero_grad()
         with torch.cuda.amp.autocast(enabled=not fp32):
             train_outputs = model(train_inputs)
             train_minibatch_loss = criterion(train_outputs, train_targets)
-        # train_minibatch_loss.backward()
         scaler.scale(train_minibatch_loss).backward()
         scaler.step(optimizer)
         scaler.update()
-        # optimizer.step()
-        # print(train_minibatch_loss.detach(), flush=True)
         train_loss += train_minibatch_loss.detach().item()
         train_acc = calculate_accuracy(train_outputs, train_targets)
         train_accuracy += train_acc
@@ -133,24 +115,12 @@ def train_valid(models, train_queue, optimizers, sample_weights, paths, weight_m
             # update weight matrix
             valid_acc = copy.deepcopy(calculate_accuracy(validation_outputs, validation_targets))
             valid_acc_batch = copy.deepcopy(valid_acc[0] / valid_acc[1])
-            # print('-------------------')
-            # print(path_index, model_index)
-            # print(valid_acc_batch)
-            # print(weight_mat)
             weight_mat[path_index, model_index] = exp_moving_avg(weight_mat[path_index, model_index], valid_acc_batch,
                                                                  decay)
-            # print(weight_mat)
             validation_loss += validation_minibatch_loss.detach().item()
             validation_accuracy += valid_acc
 
-            # # #sanity checks
-            # print('selected path:', path_index, pool)
-            # print('selected model:', model_index)
-            # print('counter:', counter_matrix)
-            # print('weight_mat:', weight_mat)
-
     return counter_matrix, weight_mat, prob_cond, prob, train_loss, validation_loss, train_accuracy, validation_accuracy
-    # return counter_matrix, weight_mat, prob_cond, prob, train_loss, validation_loss, train_accuracy, validation_accuracy
 
 
 def validate_all(models, num_models, paths, num_paths, validation_queue, prob_cond, weight_mat, fp32=False):
@@ -189,70 +159,16 @@ def validate_all(models, num_models, paths, num_paths, validation_queue, prob_co
             valid_accuracy_epoch = 0
             per_class = 0
             for batch_idx, (validation_inputs, validation_targets) in enumerate(validation_queue):
-                # validation_inputs, validation_targets = validation_inputs.cuda(), validation_targets.cuda()
                 with torch.cuda.amp.autocast(enabled=not fp32):
                     validation_outputs = model(validation_inputs)
                     valid_acc = calculate_accuracy(validation_outputs, validation_targets)
                     valid_accuracy = copy.deepcopy(valid_acc)
                     valid_accuracy_batch += valid_accuracy
-                    # per_class += accuracy_per_class(validation_outputs, validation_targets)
-                    # print(per_class.size())
             valid_acc_epoch = valid_accuracy_batch[0] / valid_accuracy_batch[1]
             print('evaluate arch', top_arch_index ,'evaluate model', top_model_index, 'with max acc', value_top20[i], 'accuracy', valid_acc_epoch, flush=True)
-                    # valid_acc_epoch_per_class = per_class[0, :] / per_class[1, :]
             init_acc_mat[top_arch_index, top_model_index] = copy.deepcopy(valid_acc_epoch)
-                    # init_acc_mat_per_class[j, i, :] = copy.deepcopy(valid_acc_epoch_per_class)
 
-            # for j in range(num_paths):
-            #     # only if max prob
-            #     if i == torch.argmax(prob_cond[j, :]):
-            #         print('evaluate model', i, 'from prob', prob_cond[j, :], flush=True)
-            #         valid_accuracy_batch = 0
-            #         valid_accuracy_epoch = 0
-            #         per_class = 0
-            #         model.module.set_path(paths[j])
-
-                    # for batch_idx, (validation_inputs, validation_targets) in enumerate(validation_queue):
-                    #     # validation_inputs, validation_targets = validation_inputs.cuda(), validation_targets.cuda()
-                    #     with torch.cuda.amp.autocast(enabled=not fp32):
-                    #         validation_outputs = model(validation_inputs)
-                    #         valid_acc = calculate_accuracy(validation_outputs, validation_targets)
-                            # valid_accuracy = copy.deepcopy(valid_acc)
-                            # valid_accuracy_batch += valid_accuracy
-                            # per_class += accuracy_per_class(validation_outputs, validation_targets)
-                        # print(per_class.size())
-                    # valid_acc_epoch = valid_accuracy_batch[0] / valid_accuracy_batch[1]
-                    # valid_acc_epoch_per_class = per_class[0, :] / per_class[1, :]
-                    # init_acc_mat[j, i] = copy.deepcopy(valid_acc_epoch)
-                    # init_acc_mat_per_class[j, i, :] = copy.deepcopy(valid_acc_epoch_per_class)
             print('validation time: ', datetime.now() - startTime, flush=True)
-
-        # for i in range(20):
-        #     top_arch_index = int(random_index[i])  # path index
-        #     top_model_index = int(model_argmax[top_arch_index])  # model index
-        #     model = models[top_model_index]  # set model
-        #     model.module.set_path(paths[top_arch_index])  # set path
-        #     model.eval()
-        #     valid_accuracy_batch = 0
-        #     valid_accuracy_epoch = 0
-        #     per_class = 0
-        # 
-        #     for batch_idx, (validation_inputs, validation_targets) in enumerate(validation_queue):
-        #         # validation_inputs, validation_targets = validation_inputs.cuda(), validation_targets.cuda()
-        #         with torch.cuda.amp.autocast(enabled=not fp32):
-        #             validation_outputs = model(validation_inputs)
-        #             valid_acc = calculate_accuracy(validation_outputs, validation_targets)
-        #             valid_accuracy = copy.deepcopy(valid_acc)
-        #             valid_accuracy_batch += valid_accuracy
-        #             # per_class += accuracy_per_class(validation_outputs, validation_targets)
-        #             # print(per_class.size())
-        #     valid_acc_epoch = valid_accuracy_batch[0] / valid_accuracy_batch[1]
-        #     print('evaluate arch', top_arch_index, 'evaluate model', top_model_index, 'with max acc',
-        #           value_rand[i], 'accuracy', valid_acc_epoch, flush=True)
-        #             # valid_acc_epoch_per_class = per_class[0, :] / per_class[1, :]
-        #     init_acc_mat[top_arch_index, top_model_index] = copy.deepcopy(valid_acc_epoch)
-        #             # init_acc_mat_per_class[j, i, :] = copy.deepcopy(valid_acc_epoch_per_class)
-        # print('validation time: ', datetime.now() - startTime, flush=True)
         print(init_acc_mat)
     return init_acc_mat, init_acc_mat_per_class
 
