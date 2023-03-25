b62427afbe17ed633b2e9164bdfc8a485a4ef0a8
mehravehj <54187772+mehravehj@users.noreply.github.com>
2023-03-25 02:28:48 -0400

    Update train_original_b.sh

--- venv/respo/cifar/resnet50/train_original_b.sh
+++ venv/respo/cifar/resnet50/train_original_b.sh
@@ -1,1 +1,1 @@
-python train_original_baseline_ffcv100.py -t $SLURM_TMPDIR/cifar100_train.beton  -v $SLURM_TMPDIR/cifar100_test.beton -j 8 -pn 204
+python train_original_baseline_ffcv100.py -t ./cifar100_train.beton  -v ./cifar100_test.beton -j 8 -pn 204
