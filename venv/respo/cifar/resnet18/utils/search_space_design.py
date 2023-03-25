ddfc222a4ca30496eae30a0b29148090ff06232b
mehravehj <54187772+mehravehj@users.noreply.github.com>
2023-03-25 02:14:09 -0400

    Update search_space_design.py

--- venv/respo/cifar/resnet18/utils/search_space_design.py
+++ venv/respo/cifar/resnet18/utils/search_space_design.py
@@ -1,5 +1,4 @@
 from itertools import combinations
-
 import torch
 from torch.distributions.categorical import Categorical as Categorical
 import numpy as np
@@ -46,11 +45,7 @@ def sample_path_prob(sample_weights, temperature):  # sample weight with a proba
     # The probs argument must be non-negative, finite and have a non-zero sum, and it will be normalized to sum to 1 along the last dimension.
     # Sample, categorical or multinomial
     prob = Categorical(logits=sample_weights * temperature)
-
-    # print('probabilities')
-    # print(prob.probs)
     path_index = int(prob.sample().data)
-    # sampled_path = paths[path_index]
     return path_index
 
 
@@ -64,16 +59,12 @@ def intialize_prob_matrix(num_paths, num_models, init_paths_w=1, init_models_w=1
     print('Initializing architecture logits with %d and model logtis with %d...' % (init_paths_w, init_paths_w))
     path_w = init_path_logit(num_paths, init_paths_w)
     weight_mat = torch.ones((num_paths, num_models)) * init_models_w
-    # weight_mat = torch.FloatTensor([[init_models_w for i in range(num_models)] for j in range(num_paths)])
     return path_w, weight_mat
 
 
 def sample_uniform(sample_weights, paths):
     prob = Categorical(logits=sample_weights)
-    # print('probabilities')
-    # print(prob.probs)
     path_index = int(prob.sample().data)
-    # sampled_path = paths[path_index]
     return path_index, paths[path_index]
 
 
@@ -99,15 +90,8 @@ def compute_marginal_prob(prob):
 
 def model_prob_calculator(weight_matrix, marginals, temperature):
     prob = torch.nn.functional.softmax(weight_matrix * temperature, dim=1)  # conditional prob p(model|arch)
-    # print(prob)
     for n in range(len(marginals)):
         p = prob / marginals[n]  # so that p is uniform across models
         sum_over_arch = torch.sum(p, 1).view(prob.size(0), 1)
         prob = p / sum_over_arch  # normalize to make sum_m(p(m|a))=1
-        # print('normalize: ', n)
-        # print('used marginal: ', marginals[n])
-        # print('marginals')
-        # print(compute_marginal_prob(prob))
-        # print('probs')
-        # print(prob)
     return prob
