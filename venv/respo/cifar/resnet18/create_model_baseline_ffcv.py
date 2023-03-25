4ede7b4a215b6a6bbd41d883f1c92c7354d848f7
mehravehj <54187772+mehravehj@users.noreply.github.com>
2023-03-25 02:19:25 -0400

    Update create_model_baseline_ffcv.py

--- venv/respo/cifar/resnet18/create_model_baseline_ffcv.py
+++ venv/respo/cifar/resnet18/create_model_baseline_ffcv.py
@@ -5,12 +5,6 @@ import torch.nn as nn
 from torch import Tensor
 import numpy as np
 
-'''
-pre-activation resnet
-resnet18
-'''
-
-
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
